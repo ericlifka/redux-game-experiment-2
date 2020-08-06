2363d92d7e255fbf4373c53d02a47b9dc634c861
Eric Lifka <eric.lifka@lifeomic.com>
2020-08-05 21:37:18 -0400

    use redux to track state

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
 import './index.scss'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from "redux"
+import thunkMiddleware from 'redux-thunk'
+import { createLogger as createLoggerMiddleware } from 'redux-logger'
 import * as THREE from 'three'
 
+const initialState = () => ({
+  rotation: { x: 0, y: 0 }
+})
+const ROTATE_ACTION = "rotate_action"
+function rotate() {
+  return {
+    type: ROTATE_ACTION
+  }
+}
+
+function update(state = initialState(), action) {
+  switch (action.type) {
+    case ROTATE_ACTION:
+      return {
+        ...state,
+        rotation: {
+          x: state.rotation.x + 0.01,
+          y: state.rotation.y + 0.01
+        }
+      }
+
+    default:
+      return state
+  }
+}
+
+const middleware = applyMiddleware(thunkMiddleware, createLoggerMiddleware())
+const { dispatch, getState, subscribe } = createStore(update, middleware)
 const { innerWidth, innerHeight } = window
 
 const scene = new THREE.Scene()
@@ -20,9 +50,12 @@ camera.position.z = 5
 
 const animate = function () {
   requestAnimationFrame( animate )
+  dispatch(rotate())
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  const state = getState()
+
+  cube.rotation.x = state.rotation.x
+  cube.rotation.y = state.rotation.y
 
   renderer.render( scene, camera )
 }
