0b9653590d293a03c538860654cfbbb47bef3f98
Eric Lifka <eric.lifka@lifeomic.com>
2020-08-05 21:05:43 -0400

    three tutorial working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
+import './index.scss'
 import { createStore } from 'redux'
-import THREE from 'three'
+import * as THREE from 'three'
 
+const { innerWidth, innerHeight } = window
 
-console.log(createStore)
-console.log(THREE)
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 )
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize( innerWidth, innerHeight )
+document.body.appendChild( renderer.domElement )
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+const cube = new THREE.Mesh( geometry, material )
+scene.add( cube )
+
+camera.position.z = 5
+
+const animate = function () {
+  requestAnimationFrame( animate )
+
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+
+  renderer.render( scene, camera )
+}
+
+animate()
